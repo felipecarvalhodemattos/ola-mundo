ba3d31d39ee698573e4e14a6dc79a4ce86baa2cf
felipecarvalhodemattos <felipecarvalhodemattos@outlook.com>
2022-08-12 19:04:11 -0300

    iniciando no php
    Primeiros passos na linguagem php

---
+++ site/ola-mundo.php
@@ -0,0 +1,39 @@
+<?php
+//  comentario de uma linha
+/*
+    comentario 
+    para mais de uma linha
+*/
+
+//  echo "olá, mundo!!";
+
+/* criando a primeira variaveis
+$idade = 21;
+echo $idade;
+*/
+
+/* criando operação matematica
+$soma = 2 + 2;
+$subtração = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$elevado = 2 ** 2;
+$restoDivisao = 2 % 3;
+
+$idade = 21; 
+$idadeDaqui10Anos = $idade + 10;
+$idadeA5Anos = $idade - 5;
+
+echo $soma;
+echo $subtração;
+echo $multiplicacao;
+echo $divisao;
+echo $elevado;
+echo $restoDivisao;
+
+echo $idadeDaqui10Anos;
+echo $idadeA5Anos;
+*/
+
+/* tipos de dados*/
+
